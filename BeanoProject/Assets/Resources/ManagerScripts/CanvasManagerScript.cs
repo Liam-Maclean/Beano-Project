80c5733efe0f463cb405d0ba1d4eabf4f5df1793
Liam-Maclean <Liamm107@hotmail.co.uk>
2017-10-17 12:47:46 +0100

    UI, Multiplayer and Foldering Changes
    Changes:
    -Added simple UI that doesn't really work (needs hooked up)
    - added in network manager that is not useful just now till gameplay is
    done
    - Added foldering for different minigames + everything outside
    minigames.

---
+++ BeanoProject/Assets/Resources/ManagerScripts/CanvasManagerScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class CanvasManagerScript : MonoBehaviour {
+
+	public Canvas SplashCanvas;
+	public Canvas MainMenuCanvas;
+	public Canvas CharacterSelectCanvas;
+	public Canvas MinigameSelectCanvas;
+	public Canvas MinigameCanvas;
+
+	void Start()
+	{
+		SplashCanvas.enabled = true;
+		MainMenuCanvas.enabled = false;
+		CharacterSelectCanvas.enabled = false;
+		MinigameSelectCanvas.enabled = false;
+		MinigameCanvas.enabled = false;
+	}
+
+
+	public void ChangeToMainMenu()
+	{
+		
+		MainMenuCanvas.enabled = true;
+		SplashCanvas.enabled = false;
+		CharacterSelectCanvas.enabled = false;
+		MinigameCanvas.enabled = false;
+		MinigameSelectCanvas.enabled = false;
+	}
+
+	public void ChangeToMinigameSelect()
+	{
+		MinigameSelectCanvas.enabled = true;
+		SplashCanvas.enabled = false;
+		MainMenuCanvas.enabled = false;
+		CharacterSelectCanvas.enabled = false;
+		MinigameCanvas.enabled = false;
+	}
+
+
+	public void ChangeToMinigame()
+	{
+		MinigameCanvas.enabled = true;
+		SplashCanvas.enabled = false;
+		MinigameSelectCanvas.enabled = false;
+		CharacterSelectCanvas.enabled = false;
+		MainMenuCanvas.enabled = false;
+	}
+
+}
