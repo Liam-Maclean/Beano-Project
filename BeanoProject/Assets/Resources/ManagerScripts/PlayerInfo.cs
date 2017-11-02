f2a0f9d8c438393a6071d2f9b5fd54fb5cd716bd
Liam-Maclean <Liamm107@hotmail.co.uk>
2017-11-02 20:17:35 +0000

    PlayerInfo
    Changes:
    -Added PlayerInfo Class for testing purposes

---
+++ BeanoProject/Assets/Resources/ManagerScripts/PlayerInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//enum for the characters chosen
+public enum Character
+{
+	none,
+	Dennis,
+	Gnasher,
+	DennisDad
+}
+	
+//to retrieve this info, GameObject.getComponent should be used
+public class PlayerInfo : MonoBehaviour{
+	//multiplayer index for each player
+	int multiplayerIndex;
+
+	//enum storing character chosen
+	Character characterChosen = Character.none;
+
+	//score of the player during and after minigames
+	int score = 0;
+
+	//earned external currency 
+	//THIS WILL LIKELY MOVE SOMEWHERE ELSE
+	int earnedCurrency = 0;
+}
