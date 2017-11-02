429e0bacae2298b44e7a5f3e72560be159b785a0
Liam-Maclean <Liamm107@hotmail.co.uk>
2017-11-02 11:28:54 +0000

    Callums UI stuff
    Changes:
    -Menu included
    -Loading screen included
    -Changes to resolution on screen
    -added assets
    -2k resolution (2540x1440)

---
+++ BeanoProject/Assets/Resources/ManagerScripts/Navigator.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigator : MonoBehaviour {
+
+    public Canvas titleCard;
+    public Canvas mainMenu;
+    public Canvas unlockRoom;
+    public Canvas settings;
+    public Canvas select;
+    
+    // Use this for initialization
+	void Start ()
+    {
+        // find all the canvasses and disable them
+        Canvas[] canvasses = FindObjectsOfType(typeof(Canvas)) as Canvas[];
+        foreach (Canvas canvas in canvasses)
+        {
+            canvas.enabled = false;
+        }
+        //enable title card
+        titleCard.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //progress from title screen
+    public void TapToStart()
+    {
+        titleCard.enabled = false;
+        mainMenu.enabled = true;
+    }
+
+    //open lobby
+    public void StartShortGame()
+    {
+        //needs variable for game length added
+        PlayerPrefs.SetInt("Game Length", 5);                       //Variable added, will look at other ways to do this; it is not automatically zeroed on game start. This can be added or it may not be
+        SceneManager.LoadScene("Loading", LoadSceneMode.Single);    // necessary, depending on if and how it is handled during actual gameplay
+    }
+
+    //open lobby
+    public void StartLongGame()
+    {
+        //needs variable for game length added
+        PlayerPrefs.SetInt("Game Length", 20);                      //Variable added
+        SceneManager.LoadScene("Loading", LoadSceneMode.Single);
+    }
+
+    //go from unlock screen to main menu
+    public void ReturnFromUnlock()
+    {
+        unlockRoom.enabled = false;
+        mainMenu.enabled = true;
+    }
+
+    //go to unlock screen from main menu
+    public void EnterUnlock()
+    {
+        mainMenu.enabled = false;
+        unlockRoom.enabled = true;
+    }
+
+    //open settings screen from main menu
+    public void Settings()
+    {
+        settings.enabled = true;
+        mainMenu.enabled = false;
+    }
+
+    //return to menu from settings screen
+    public void ReturnFromSettings()
+    {
+        mainMenu.enabled = true;
+        settings.enabled = false;
+    }
+
+    //go to level select for dev testing
+    public void EnterLevelSelect()
+    {
+        select.enabled = true;
+        mainMenu.enabled = false;
+    }
+}
