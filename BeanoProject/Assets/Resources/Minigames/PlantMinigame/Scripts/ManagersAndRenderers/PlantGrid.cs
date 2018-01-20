b0f9a5426149d34a62744f8eb0bf793ec97bf891
Liam-Maclean <Liamm107@hotmail.co.uk>
2018-01-20 18:40:52 +0000

    Plant Minigame Push
    -Added numerical text pop when plants destroyed.
    -Added size pop to players score
    -Added refined Art: Background, 4 new plant sprites and timer
    -Added timer with no functionality
    -Added animation scripts and animation files for both text pop and score pop

--- BeanoProject/Assets/Resources/Minigames/PlantMinigame/Scripts/ManagersAndRenderers/PlantGrid.cs
+++ BeanoProject/Assets/Resources/Minigames/PlantMinigame/Scripts/ManagersAndRenderers/PlantGrid.cs
@@ -2,9 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//==============================================
+//
+// Plant grid Script
+//
+// This class creates the plant grid in formation
+//
+// Move the parent "Garden" gameobject in inspector to move plant grid
+// 
+//
+// 
+// 
+//
+// Liam MacLean - Edited 20/01/2018 17:46
+
 
 public class PlantGrid : MonoBehaviour{
-
 	private GameObject [,] m_tileGrid;
 	private GameObject[,] m_plantGrid;
 	private GameObject parent;
@@ -13,24 +26,29 @@ public class PlantGrid : MonoBehaviour{
 
 	public void CreateGrd(int width, int height, int backgroundWidth, int backgroundHeight)
 	{
-		m_tileGrid = new GameObject[backgroundWidth,backgroundHeight];
-		parent = new GameObject ("Tile Parent");
-		for (int y = 0; y < backgroundHeight; y++)
-		{
-			for (int x = 0; x < backgroundWidth; x++)
-			{
-				m_tileGrid [y,x] = Instantiate ((GameObject)Resources.Load ("Minigames/PlantMinigame/Prefabs/BackgroundTiles"), new Vector3 (x*1.5f, y*1.5f, 0), Quaternion.identity);
-				m_tileGrid [y, x].transform.SetParent (parent.transform);
+		//***REDUNDANT CODE*** 
+
+		//m_tileGrid = new GameObject[backgroundWidth,backgroundHeight];
+		//parent = new GameObject ("Tile Parent");
+		//for (int y = 0; y < backgroundHeight; y++)
+		//{
+		//	for (int x = 0; x < backgroundWidth; x++)
+		//	{
+		//		m_tileGrid [y,x] = Instantiate ((GameObject)Resources.Load ("Minigames/PlantMinigame/Prefabs/BackgroundTiles"), new Vector3 (x*1.5f, y*1.5f, 0), Quaternion.identity);
+		//		m_tileGrid [y, x].transform.SetParent (parent.transform);
+		//
+		//
+        //        if (y == backgroundHeight - 9)
+        //        {
+        //            m_tileGrid[y, x].GetComponent<TileTextureManager>().SetTexture(6);   
+        //        }
+        //    }
+		//}
+		//parent.transform.position =  new Vector3 (-(backgroundWidth / 2), -(backgroundHeight / 2), 0.0f);
 
 
-                if (y == backgroundHeight - 9)
-                {
-                    m_tileGrid[y, x].GetComponent<TileTextureManager>().SetTexture(6);   
-                }
-            }
-		}
 
-		parent.transform.position =  new Vector3 (-(backgroundWidth / 2), -(backgroundHeight / 2), 0.0f);
+
 
 		m_plantGrid = new GameObject[height,width];
 		plantParent = GameObject.Find("PlantParent");
@@ -45,7 +63,7 @@ public class PlantGrid : MonoBehaviour{
                 //Sets the parent of the instantiated Grid object, to the Tile parent (you can move this Gameobject in the editor to move the whole tilegrid)
                 m_plantGrid [y,x] = Instantiate ((GameObject)Resources.Load ("Minigames/PlantMinigame/Prefabs/pTile"));
                 m_plantGrid[y, x].transform.localPosition = plantParent.transform.position;
-                m_plantGrid[y, x].transform.localPosition = new Vector3(plantParent.transform.position.x + (x * 1.2f), plantParent.transform.position.y + (y * 1.2f), -2);
+				m_plantGrid[y, x].transform.localPosition = new Vector3(plantParent.transform.position.x + (x * 1.2f), plantParent.transform.position.y + (y * 1.2f), -2 + (y*0.1f));
                 m_plantGrid[y, x].transform.parent = plantParent.transform;
             }
 		}
