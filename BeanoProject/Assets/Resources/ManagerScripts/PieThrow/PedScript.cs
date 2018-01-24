ed69be6f604cd546aa98fd6a44f9118177b6e03f
ashleydouglas-abertay <1503347@uad.ac.uk>
2018-01-24 13:51:51 +0000

    Update1
    +started new class for peds
    +added PH's
    +Removed old peds
    +Creeated new manager

---
+++ BeanoProject/Assets/Resources/ManagerScripts/PieThrow/PedScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedScript : MonoBehaviour
+{
+    private enum
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ BeanoProject/Assets/Resources/ManagerScripts/PieThrow/PieThrowManagerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieThrowManagerScript : MonoBehaviour
+{
+    private int m_clientID;
+    //public GameObject[] m_portraits;
+
+    public GameObject readyMenu;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Set Client ID
+        // Set Portraits
+        // Set Powerup state
+
+        StartMenu();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    // Called at the begining of the game to make sure all users are loaded into the game correctly
+    void StartMenu()
+    {
+
+    }
+
+    // Called to start the active game & timers
+    void StartGame()
+    {
+
+    }
+}
