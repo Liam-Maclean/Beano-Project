2901177a77aa7770daceeee80537a8d2a698a783
Oswald-Maximus <32834939+Oswald-Maximus@users.noreply.github.com>
2018-01-24 16:10:56 +0000

    more network progress

---
+++ BeanoProject/Assets/Resources/ManagerScripts/Network/LobbyScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LobbyScript : NetworkLobbyPlayer {
+
+    Networker networker;
+
+    // Use this for initialization
+    void Start () {
+       string handle = networker.handle;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
