59fd529759cccf4ebbb50eae768e401c9a923cf5
ashleydouglas-abertay <1503347@uad.ac.uk>
2018-01-31 17:15:56 +0000

    Box Goes Boom
    -Menu gets big

--- BeanoProject/Assets/Resources/ManagerScripts/PieThrow/PieThrowManagerScript.cs
+++ BeanoProject/Assets/Resources/ManagerScripts/PieThrow/PieThrowManagerScript.cs
@@ -6,20 +6,14 @@ public class PieThrowManagerScript : MonoBehaviour
 {
     private enum GAMESTATE { Start, Playing, Finished};
     private GAMESTATE m_currState;
-    private int m_clientID;
+    //private int m_clientID;
 
-    public Sprite[] AllPortraits;
-
-    private List<GameObject> m_menus;
-
-    public GameObject MenuBackPrefab;
-    public GameObject MenuButtonPrefab;
+    public Animator readyMenuAnim;
 
     public GameObject readyMenu;
 
     void Awake()
     {
-        m_menus = new List<GameObject>();
         m_currState = GAMESTATE.Start;
     }
 
@@ -57,22 +51,17 @@ public class PieThrowManagerScript : MonoBehaviour
     // Called at the begining of the game to make sure all users are loaded into the game correctly
     void StartMenu()
     {
-        new WaitForSeconds(1.5f);
-
-        GameObject newMenuItem;
-
-        newMenuItem = (GameObject)Instantiate(MenuBackPrefab);
-        m_menus.Add(newMenuItem);
-
-        newMenuItem = (GameObject)Instantiate(MenuButtonPrefab);
-        m_menus.Add(newMenuItem);
-
-        // ACTICATE BUTTON LOGIC (AAKA COLLISIONS ETC BRUDDA KNOW DA WAY)
+        readyMenuAnim.SetBool("Active", true);
     }
 
     // Called to start the active game & timers
     void StartGame()
     {
+        
+    }
 
+    IEnumerator Wait(float duration)
+    {
+        yield return new WaitForSeconds(duration);
     }
 }
