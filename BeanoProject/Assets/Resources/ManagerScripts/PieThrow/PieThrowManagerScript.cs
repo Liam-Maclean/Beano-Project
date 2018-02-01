f95cc5665f6b71476cd94bab1c3de0eb636209b5
crazymanash <crazymanash@users.noreply.github.com>
2018-02-01 12:37:50 +0000

    Spawner Commit
    -Movemnt an issue and will need to rotate in next update

--- BeanoProject/Assets/Resources/ManagerScripts/PieThrow/PieThrowManagerScript.cs
+++ BeanoProject/Assets/Resources/ManagerScripts/PieThrow/PieThrowManagerScript.cs
@@ -6,15 +6,42 @@ public class PieThrowManagerScript : MonoBehaviour
 {
     private enum GAMESTATE { Start, Playing, Finished};
     private GAMESTATE m_currState;
-    //private int m_clientID;
+
+    public int minZDist;
+    public int maxZDist;
+
+    public float spawnRateMin;
+    public float spawnRateMax;
+    private float[] m_spawnTimer;
+    private float[] m_spawnRateRand;
+
+    public float aircraftOdds;
+
+    public int basicPedTypes;
+    public int specialTypes;
+
+    public Vector2 targetPos;
+    public float xFlipDistance;
+
+    public GameObject[] pedPrefabs;
+    private List<GameObject> m_pedObjects;
 
     public Animator readyMenuAnim;
-
     public GameObject readyMenu;
 
     void Awake()
     {
         m_currState = GAMESTATE.Start;
+
+        m_pedObjects = new List<GameObject>();
+
+        m_spawnTimer = new float[maxZDist - minZDist];
+        m_spawnRateRand = new float[maxZDist - minZDist];
+
+        for (int i = 0; i < maxZDist - minZDist; i++)
+        {
+            m_spawnRateRand[i] = Random.Range(spawnRateMin, spawnRateMax);
+        }
     }
 
 	// Use this for initialization
@@ -46,6 +73,47 @@ public class PieThrowManagerScript : MonoBehaviour
                 Debug.Log("GameState Error");
                 break;
         }
+
+        // MOVED FOR TESTING
+
+        for (int i = 0; i < maxZDist - minZDist; i++)
+        {
+            if (m_spawnTimer[i] >= m_spawnRateRand[i])
+            {
+                m_spawnTimer[i] -= m_spawnRateRand[i];
+                m_spawnRateRand[i] = Random.Range(spawnRateMin, spawnRateMax);
+
+                float threshold = Random.Range(0.00f, 1.00f);
+                int isLeft = Random.Range(0, 1);
+
+                if (threshold > aircraftOdds)
+                {
+                    //PED
+                    if (isLeft == 0)
+                    {
+                        SpawnPed(true, false, i + minZDist);
+                    }
+                    else
+                    {
+                        SpawnPed(true, true, i + minZDist);
+                    }
+                }
+                else
+                {
+                    //PLANE
+                    if (isLeft == 0)
+                    {
+                        SpawnPed(false, false, i + minZDist);
+                    }
+                    else
+                    {
+                        SpawnPed(false, true, i + minZDist);
+                    }
+                }
+            }
+
+            m_spawnTimer[i] += Time.deltaTime;
+        }
 	}
 
     // Called at the begining of the game to make sure all users are loaded into the game correctly
@@ -60,8 +128,31 @@ public class PieThrowManagerScript : MonoBehaviour
         
     }
 
-    IEnumerator Wait(float duration)
+    void SpawnPed(bool isBasic, bool isLeft, int zPos)
     {
-        yield return new WaitForSeconds(duration);
+        GameObject newPed;
+        int typeHelper = 0;
+
+        if (isBasic)
+        {
+            typeHelper = Random.Range(0, basicPedTypes);
+        }
+        else
+        {
+            typeHelper = Random.Range(0, specialTypes);
+            typeHelper += basicPedTypes;
+        }
+
+        if (isLeft)
+        {
+            newPed = (GameObject)Instantiate(pedPrefabs[typeHelper], new Vector3(targetPos.x, targetPos.y, zPos), Quaternion.identity);
+        }
+        else
+        {
+            newPed = (GameObject)Instantiate(pedPrefabs[typeHelper], new Vector3(targetPos.x + xFlipDistance, targetPos.y, zPos), Quaternion.identity);
+        }
+
+        m_pedObjects.Add(newPed);
+        m_pedObjects[m_pedObjects.Count - 1].GetComponent<PedScript>().InitPed(isLeft, zPos);
     }
 }
