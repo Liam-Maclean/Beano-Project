47af73c61029d91a0501f06a5601315c3a6b473f
Liam-Maclean <Liamm107@hotmail.co.uk>
2018-02-06 21:07:02 +0000

    Mole Spawning
    -Added Mole Spawning
    -Added functionality for multiple types of moles
    -Added current functionality of Human and Mole type moles with different derived classes
    -Added placeholder plant art to mole spawning

---
+++ BeanoProject/Assets/Resources/Minigames/GnasherMole/Scripts/Mole.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Mole class
+/// 
+/// 
+/// 
+///	Hit these "moles" and you'll get positive score
+/// 
+/// 
+/// 
+/// Liam MacLean 20:09 06/02/2018
+/// </summary>
+public class Mole : BaseMole {
+
+	//Initialises on instantiation
+	void Start()
+	{
+		SetScore (1);
+		SetDuration(Random.Range(0.5f, 2.0f));
+	}
+}
