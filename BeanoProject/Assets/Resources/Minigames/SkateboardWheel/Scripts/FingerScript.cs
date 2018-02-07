13a90ed49865c45dba2e4fcbb4803931bef7e8c2
Oswald-Maximus <32834939+Oswald-Maximus@users.noreply.github.com>
2018-02-07 22:24:07 +0000

    add Skateboard Wheel minigame. Add Physics Objects for Skateboard Wheel minigame

---
+++ BeanoProject/Assets/Resources/Minigames/SkateboardWheel/Scripts/FingerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerScript : MonoBehaviour {
+
+    public GameObject finger;
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 fingerPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).x, Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).y);
+        finger.transform.position = fingerPos;
+		
+	}
+}
