8d842f93a6187d8ee843b315f92f08f32ad893ac
Oswald-Maximus <32834939+Oswald-Maximus@users.noreply.github.com>
2018-02-08 08:15:23 +0000

    Begin scoring system for Skateboard Wheel

---
+++ BeanoProject/Assets/Resources/Minigames/SkateboardWheel/Scripts/ScoreScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour {
+
+    int scoreChange;
+    bool direction; // true for clockwise
+    int i;
+    int scoreMultiplier;
+    float prevRot;
+    public GameObject wrench;
+
+	// Use this for initialization
+	void Start () {
+        i = 0;
+        direction = true;
+        scoreMultiplier = 1;
+        prevRot = -90;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreChange = 0;
+        scoreChange = Mathf.RoundToInt(wrench.transform.rotation.z - prevRot);
+        scoreChange *= scoreMultiplier;
+        if (i > (Random.value * 1000))
+        {
+            direction = !direction;
+            scoreMultiplier *= -1;
+            i = 0;
+        }
+        else i++;
+        GameScript.local.Score(scoreChange);
+	}
+}
