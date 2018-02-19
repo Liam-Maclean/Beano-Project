f9cf034d58d1567e0bce6b1a1c417716ba699ed3
crazymanash <crazymanash@users.noreply.github.com>
2018-02-19 01:25:56 +0000

    Overworld Update
    -Updated Background (still PH)
    -Node Update (removed z movement)
    -Updated Node positioning (Not Final)
    -Rescaled player prefabs
    -Fixing camera control (Untesed)

--- BeanoProject/Assets/Resources/ManagerScripts/Overworld/CameraScript.cs
+++ BeanoProject/Assets/Resources/ManagerScripts/Overworld/CameraScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
+    public float zoomSpeed;
+
     public float targetZoom;
     public Vector3 targetPos;
     public Camera orthoCamera;
@@ -14,51 +16,71 @@ public class CameraScript : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 currPos = transform.position;
-
-        if (currPos.x <= targetPos.x - newSpaceBuffer || currPos.x >= targetPos.x + newSpaceBuffer || currPos.y <= targetPos.y - newSpaceBuffer || currPos.y >= targetPos.y + newSpaceBuffer)
+        if (Input.touchCount == 2)
         {
-            m_moveVelo.x = targetPos.x - currPos.x;
-            m_moveVelo.y = targetPos.y - currPos.y;
-            m_moveVelo.Normalize();
+            Touch touch0 = Input.GetTouch(0);
+            Touch touch1 = Input.GetTouch(1);
 
-            if (m_momentum == 0)
-            {
-                m_momentum = 0.1f;
-            }
-            else
-            {
-                if (m_momentum < 0.75f)
-                {
-                    m_momentum = m_momentum * 1.25f;
-                }
-                else
-                {
-                    m_momentum = 1.0f;
-                }
-            }
+            Vector2 touchOldPos0 = touch0.position - touch0.deltaPosition;
+            Vector2 touchOldPos1 = touch1.position - touch1.deltaPosition;
 
-            transform.position += (m_moveVelo * m_momentum * Time.deltaTime);
+            float oldTouchMag = (touchOldPos0 - touchOldPos1).magnitude;
+            float currTouchMag = (touch0.position - touch1.position).magnitude;
+
+            float magDifference = oldTouchMag - currTouchMag;
+
+            orthoCamera.orthographicSize += magDifference * zoomSpeed;
+            orthoCamera.orthographicSize = Mathf.Max(orthoCamera.orthographicSize, 0.1f);
         }
         else
         {
-            m_momentum = 0;
-        }
 
-        float currSize = orthoCamera.orthographicSize;
+            Vector3 currPos = transform.position;
 
-        if (currSize != targetZoom)
-        {
-            if (targetZoom > currSize && currSize < 10)
+            if (currPos.x <= targetPos.x - newSpaceBuffer || currPos.x >= targetPos.x + newSpaceBuffer || currPos.y <= targetPos.y - newSpaceBuffer || currPos.y >= targetPos.y + newSpaceBuffer)
             {
-                currSize += 0.01f;
+                m_moveVelo.x = targetPos.x - currPos.x;
+                m_moveVelo.y = targetPos.y - currPos.y;
+                m_moveVelo.Normalize();
+
+                if (m_momentum == 0)
+                {
+                    m_momentum = 0.1f;
+                }
+                else
+                {
+                    if (m_momentum < 0.75f)
+                    {
+                        m_momentum = m_momentum * 1.25f;
+                    }
+                    else
+                    {
+                        m_momentum = 1.0f;
+                    }
+                }
+
+                transform.position += (m_moveVelo * m_momentum * Time.deltaTime);
             }
-            else if (currSize > 3)
+            else
             {
-                currSize -= 0.01f;
+                m_momentum = 0;
             }
 
-            orthoCamera.orthographicSize = currSize;
+            float currSize = orthoCamera.orthographicSize;
+
+            if (currSize != targetZoom)
+            {
+                if (targetZoom > currSize && currSize < 10)
+                {
+                    currSize += 0.01f;
+                }
+                else if (currSize > 3)
+                {
+                    currSize -= 0.01f;
+                }
+
+                orthoCamera.orthographicSize = currSize;
+            }
         }
     }
 
