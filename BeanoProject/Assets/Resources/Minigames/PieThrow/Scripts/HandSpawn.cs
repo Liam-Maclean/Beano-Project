cbf73a28e898dc97a16bfae9935e1c816560f95f
Kevin-Conaghan <kevin_conaghan@hotmail.co.uk>
2018-02-21 13:22:09 +0000

    Hand Finalised
    Touch controls have been implemented for the hand and is commented.

--- BeanoProject/Assets/Resources/Minigames/PieThrow/Scripts/HandSpawn.cs
+++ BeanoProject/Assets/Resources/Minigames/PieThrow/Scripts/HandSpawn.cs
@@ -2,58 +2,85 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <This script deals with spawning the hand>
+/// Hand spawn.
+/// this script will be attatched to an empty game object that will spawn the hand prefab.
+/// It spawns a hand prefab at the current mouse position and it moves depending on the current
+/// mouse or touch position.
+/// <This will be the final version>
+
+
 public class HandSpawn : MonoBehaviour
 { 
-    private SpriteRenderer sr;
-    private bool oldMouseDown;
-    private bool newMouseDown;
-    public Sprite handClosed;
-    public Sprite handOpen;
+	//gameObject variables
     public GameObject handPrefab;
     private GameObject clone;
+
+	//vector2 variables
+	private Vector2 m_touch;
 
 
     // Use this for initialization
     void Start()
     {
-        sr = GetComponent<SpriteRenderer>();
-        
+		//get the current mouse position
+		Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		//spawn the hand at the current mouse position
+		clone = (GameObject)Instantiate(handPrefab, mousePos, Quaternion.identity);
+		//set the cursor to invisible
+		Cursor.visible = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        newMouseDown = Input.GetMouseButtonDown(0);
-
-        if (newMouseDown && !oldMouseDown)
-        {
-            clone = (GameObject)Instantiate(handPrefab, mousePos, Quaternion.identity);
-           
-        }
-
-        clone.transform.position = mousePos;
-        
-        if (!newMouseDown && oldMouseDown)
-        {
-            Destroy(clone);
-        }
-
-        oldMouseDown = newMouseDown;
-
+		//this handles all the mouse controls
+		MouseControls ();
+		//this handles all the touch controls
+		TouchControls ();
     }
 
 
-    void OnCollisionEnter(Collision collisionInfo)
-    {
-        Debug.Log("hit");
+	void MouseControls()
+	{
+		//get the current mouse position which is updated every frame
+		Vector2 currMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+		//if the hand exists
+		if (clone)
+		{
+			//set the hands position to the current mouse position
+			clone.transform.position = currMousePos;
+		}
+	}
 
-        if (collisionInfo.collider.tag == "pie")
-        {
-            sr.sprite = handClosed;
-        }
+	void TouchControls()
+	{
+		//get the world position of the touch
+		Vector2 currTouchPos = Camera.main.ScreenToWorldPoint (m_touch);
 
-    }
+		//Touch Controls
+		if (Input.touchCount == 1) 
+		{
+			Touch touch = Input.GetTouch (0);
+
+			//Switch statement determining which type of touch it is
+			switch (touch.phase) 
+			{
+			case TouchPhase.Began:
+				//store the initial position
+				if (clone)
+				{
+					//set the hands position to the current touch position 
+					clone.transform.position = currTouchPos;
+				};
+				break;
+			case TouchPhase.Moved:
+				//set the current touch position
+				m_touch = touch.position;
+				break;
+			}
+		}
+	}
 }
