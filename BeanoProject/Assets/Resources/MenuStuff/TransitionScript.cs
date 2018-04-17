693220017fea9f463cf289f448d48608c36d2b08
Liam-Maclean <Liamm107@hotmail.co.uk>
2018-04-17 02:14:25 +0100

    Transitions, Menu art and behaviour scripts
    -Added transition prefab for swapping scenes (pass it a scene name and a loadtype and it will load halfway through a splat with a transition)
    -Added behaviour cleanup to destroy animation after it has ended
    -Added in transition assets
    -Added join and host assets for loading scene

---
+++ BeanoProject/Assets/Resources/MenuStuff/TransitionScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionScript : MonoBehaviour {
+
+	StopAnimationScript stopAnimScript;
+	// Use this for initialization
+	void Start () {
+		stopAnimScript = this.GetComponent<StopAnimationScript> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (stopAnimScript.AnimationEnded ()) {
+		//	Destroy (this.gameObject);
+		//}
+	}
+}
---
+++ BeanoProject/Assets/SwapSceneBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwapSceneBehaviour : StateMachineBehaviour {
+
+	GameObject thisGameObject;
+	string sceneToTransitionTo;
+	LoadSceneMode loadMode;
+
+	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		thisGameObject = GameObject.FindGameObjectWithTag ("Transition");
+		sceneToTransitionTo = thisGameObject.GetComponent<TransitionManagerScript> ().GetSceneNumberToChangeTo();
+		loadMode = thisGameObject.GetComponent<TransitionManagerScript> ().GetLoadSceneMode ();
+
+		Debug.Log (loadMode);
+		Debug.Log(sceneToTransitionTo);
+		SceneManager.LoadScene (sceneToTransitionTo, loadMode);
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
